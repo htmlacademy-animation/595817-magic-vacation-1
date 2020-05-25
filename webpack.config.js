9bcd1049c807aba53736299b189cf06a65a0da16
Irina <irabulich@gmail.com>
2020-05-25 23:02:20 +0300

    Исправления к - Анимируем фон при переключении экранов

--- webpack.config.js
+++ webpack.config.js
@@ -29,9 +29,11 @@ module.exports = {
     filename: 'js/script.js'
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.s[a|c]ss$/,
-        use: [{
+        use: [
+          {
             loader: MiniCssExtractPlugin.loader,
           },
           {
@@ -43,15 +45,14 @@ module.exports = {
           },
           {
             loader: "sass-loader",
-            options: {
-              sourceMap: true
-            },
+            options: { sourceMap: true },
           }
         ],
       },
       {
         test: /\.css$/,
-        use: [{
+        use: [
+          {
             loader: MiniCssExtractPlugin.loader,
           },
           {
@@ -65,7 +66,8 @@ module.exports = {
       },
       {
         test: /\.html$/,
-        use: [{
+        use: [
+          {
             loader: 'html-loader',
             options: {
               attributes: false
@@ -75,9 +77,7 @@ module.exports = {
             loader: 'posthtml-loader',
             options: {
               plugins: [
-                require('posthtml-include')({
-                  root: 'source'
-                })
+                require('posthtml-include')({ root: 'source' })
               ]
             }
           }
@@ -98,7 +98,8 @@ module.exports = {
     new CssoWebpackPlugin({
       pluginOutputPostfix: 'min'
     }),
-    new CopyPlugin([{
+    new CopyPlugin([
+      {
         from: "source/fonts/**/*.{woff,woff2}",
         to: path.join(__dirname, 'build', 'fonts'),
         flatten: true,
